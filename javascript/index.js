7e5f35a04e59de299ca985a3403c86504b176aaf
dalysoncarv <dalyson.carv@gmail.com>
2025-03-01 10:14:40 -0300

    Alterações em style.css e index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,32 +1,26 @@
-const themeToggleBtn = document.getElementById('theme-toggle');
+const themeToggleButton = document.getElementById("theme-toggle");
 const body = document.body;
-const sunIcon = document.getElementById('sun-icon');
-const moonIcon = document.getElementById('moon-icon');
+const sunIcon = document.getElementById("sun-icon");
+const moonIcon = document.getElementById("moon-icon");
 
+themeToggleButton.addEventListener("click", () => {
+    body.classList.toggle("dark-theme");
 
-if (localStorage.getItem('theme') === 'dark') {
-    body.classList.add('dark-theme');
-    moonIcon.style.display = 'inline-block';
-    sunIcon.style.display = 'none';
-} else {
-    body.classList.add('light-theme');
-    moonIcon.style.display = 'none';
-    sunIcon.style.display = 'inline-block';
-}
-
-
-themeToggleBtn.addEventListener('click', () => {
-    if (body.classList.contains('light-theme')) {
-        body.classList.remove('light-theme');
-        body.classList.add('dark-theme');
-        sunIcon.style.display = 'none';
-        moonIcon.style.display = 'inline-block';
-        localStorage.setItem('theme', 'dark'); 
-    } else {
-        body.classList.remove('dark-theme');
-        body.classList.add('light-theme');
-        sunIcon.style.display = 'inline-block';
-        moonIcon.style.display = 'none';
-        localStorage.setItem('theme', 'light'); 
-    }
+    const isDarkTheme = body.classList.contains("dark-theme");
+    
+    sunIcon.style.display = isDarkTheme ? "none" : "inline";
+    moonIcon.style.display = isDarkTheme ? "inline" : "none";
+    
+    localStorage.setItem("theme", isDarkTheme ? "dark" : "light");
 });
+
+
+if (localStorage.getItem("theme") === "dark") {
+    body.classList.add("dark-theme");
+    sunIcon.style.display = "none";
+    moonIcon.style.display = "inline";
+} else {
+    body.classList.add("light-theme");
+    sunIcon.style.display = "inline";
+    moonIcon.style.display = "none";
+}
